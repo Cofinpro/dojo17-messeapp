ecd01a54b7ca9f393b9d6dbbcb08a9d4bf0ebeea
Patrick Wade <patrickwadee@googlemail.com>
2017-09-08 16:58:54 +0200

    solved conflicts

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const fieldValidator = require('./modules/validator');
-
-const server = app.listen(3000, function () {
-    console.log('Server läuft ');
-});
-
-app.get('/', (req, res)=> {
-    res.send('What can i do with the ');
-});
-
-// Add new entry to database
-app.post('/add', (req, res)=> {
-
-});
-
-// Update existing entry
-app.post('/update', (req, res)=> {
-
-});
-
-// Add rating to entry
-app.post('/rate', (req, res)=> {
-
-});
-
-// Export data
-app.post('/export', (req, res)=> {
-
-});
-=======
 const express = require('express');
 const bodyParser = require('body-parser');
 const _ = require('lodash');
@@ -49,8 +15,8 @@ app.get('/', (req, res)=> {
 
 // Add new entry to database
 app.post('/add', (req, res)=> {
-    
-    checkIncoming.isValid(data); 
+
+    checkIncoming.isValid(data);
 });
 
 // Update existing entry
@@ -67,4 +33,3 @@ app.post('/rate', (req, res)=> {
 app.post('/export', (req, res)=> {
 
 });
->>>>>>> abb59b1e5c43cce492bc80f92725236ad4c10dbc
