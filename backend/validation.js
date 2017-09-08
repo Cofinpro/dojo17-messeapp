d7b19747e82e1411d72700421511128670d16bd7
Patrick Wade <patrickwadee@googlemail.com>
2017-09-08 15:44:01 +0200

    Addition to validation.js

--- backend/validation.js
+++ backend/validation.js
@@ -1,30 +1,27 @@
 var fomularData = {
-        //Anrede
-        firstName:  "John",
-        lastName:   "Doe",
-        university: "Hochschule Darmstadt",
-        course:     "Informatik",
-        lastDegree: "Bachelor of Science Informatik",
-        lastDegreeDate:"01.01.2017",
-        eMail:      "John@doe.com",
-        telephoneNumber: "0177/3432432424"
-    };
+  //Anrede
+  firstName: "Joe",
+  lastName: "Doe",
+  university: "Hochschule Darmstadt",
+  course: "Informatik",
+  lastDegree: "Bachelor of Science Informatik",
+  lastDegreeDate: "01.01.2017",
+  eMail: "John@doe.com",
+  telephoneNumber: "0177/3432432424"
+};
 
-var validatorFunction = function(fomularData) {
+var validatorFunction = function (fomularData) {
 
-    //Check for empty values
-    for (var o in fomularData){
-        o.tri
-    }
-        if (fomularData[o] === "") return false;
+  //Check for empty values
+  for (var o in fomularData) {
+    if (fomularData[o].trim() === "" || String(fomularData[o]).length < 3) return false;
+  }
 
-    var testString = "woword";
+  //Values that should only be alphabetic
+  if (/[^a-zA-z ]/.test(fomularData.lastDegree))
+    console.log("incorrect");
 
-    //Values that should only be alphabetic
-    if(/[^a-zA-z ]/.test(fomularData.lastDegree))
-        console.log("incorrect");
-
-    return true;
+  return true;
 };
 
 
