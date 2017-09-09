69fed2cdc1c28fe98369555bd970bf855386432f
Patrick Wade <patrickwadee@googlemail.com>
2017-09-09 10:55:40 +0200

    Cleaned Validator- Finished Json2Xls- Added packages

--- backend/modules/validator.js
+++ backend/modules/validator.js
@@ -7,31 +7,10 @@ var validatorFunction = function (fomularData) {
     return false;
   }
 
-  //Values that shouldn´t contain numbers
-  //Not used because the fields doesnt need to be filled
-  //if (/[0-9]/.test(fomularData.salutation) ||
-  //    /[0-9]/.test(fomularData.firstname) ||
-  //    /[0-9]/.test(fomularData.name) ||
-  //    /[0-9]/.test(fomularData.university) ||
-  //    /[0-9]/.test(fomularData.course) ||
-  //    /[0-9]/.test(fomularData.graduation) ||
-  //    /[0-9]/.test(fomularData.interest.exam ||
-  //    /[0-9]/.test(fomularData.interest.student) ||
-  //    /[0-9]/.test(fomularData.interest.dhbw) ||
-  //    /[0-9]/.test(fomularData.boarding))) {
-  //  return false;
-  //}
-
   //Check Email
   if(!(/[@]/.test(fomularData.email))) {
     return false;
   }
-
-  //Check number
-  //Not used because the fields doesnt need to be filled
-  //if(/3[a-zA-Z]/.test(fomularData.graduation_date) || /[a-zA-Z]/.test(fomularData.telephone)) {
-  //  return false;
-  //}
 
   return true;
 };
