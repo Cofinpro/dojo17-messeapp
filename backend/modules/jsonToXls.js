4cf40cf3ca1dca27e7b666b3775d02e4262a8d6e
Patrick Wade <patrickwadee@googlemail.com>
2017-09-09 12:49:26 +0200

    jsonToXls

--- backend/modules/jsonToXls.js
+++ backend/modules/jsonToXls.js
@@ -1,11 +1,55 @@
 var json2xls = require('json2xls');
 var fs = require('fs');
 var moment = require('moment');
-_ = require('lodash');
+var _ = require('lodash');
+var nodeExcel = require('excel-export');
 
-var options = {
+var createStyle = function() {
+  var conf = {};
+  conf.stylesXmlFile = "styles.xml";
+  conf.name = "mysheet";
+  conf.cols = [{
+    caption: 'string',
+    type: 'string',
+    beforeCellWrite: function (row, cellData) {
+      return cellData.toUpperCase();
+    },
+    width: 28.7109375
+  }, {
+    caption: 'date',
+    type: 'date',
+    beforeCellWrite: function () {
+      var originDate = new Date(Date.UTC(1899, 11, 30));
+      return function (row, cellData, eOpt) {
+        if (eOpt.rowNum % 2) {
+          eOpt.styleIndex = 1;
+        }
+        else {
+          eOpt.styleIndex = 2;
+        }
+        if (cellData === null) {
+          eOpt.cellType = 'string';
+          return 'N/A';
+        } else
+          return (cellData - originDate) / (24 * 60 * 60 * 1000);
+      }
+    }()
+  }, {
+    caption: 'bool',
+    type: 'bool'
+  }, {
+    caption: 'number',
+    type: 'number'
+  }];
+  conf.rows = [
+    ['pi', new Date(Date.UTC(2013, 4, 1)), true, 3.14],
+    ["e", new Date(2012, 4, 1), false, 2.7182],
+    ["M&M<>'", new Date(Date.UTC(2013, 6, 9)), false, 1.61803],
+    ["null date", null, true, 1.414]
+  ];
 
-}
+  return conf;
+};
 
 var SaveJsonAsXls = function(jsonArr)
 {
@@ -50,7 +94,9 @@ var SaveJsonAsXls = function(jsonArr)
     delete(value.__v);
   });
 
-  var xls = json2xls(jsonArr);
+  var xls = json2xls(jsonArr,{
+    fields: { salutation : 'string'}
+  });
 
   fs.writeFileSync('../export/data.xlsx', xls, 'binary');
 
